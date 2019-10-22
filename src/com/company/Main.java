8b07c29d1e83178577e7f79753869a036de29fc6
Beverly Ann Gupit <Bevzay@users.noreply.github.com>
2019-10-23 00:12:07 +1300

    Strings

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        String myString = "This is my String";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + ", and this is more";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + " \u00A9 2019";
+        System.out.println("myString is equal to " + myString);
+
     }
 }
